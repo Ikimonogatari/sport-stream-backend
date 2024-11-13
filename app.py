834140393c4078e6b97f85c89b16ad8fe886277a
bbattulga <battulga.dev@gmail.com>
2024-11-13 22:47:25 +0800

    remove crawler

--- app.py
+++ app.py
@@ -1,32 +1,31 @@
 from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS
 from os import environ
-import logging
 from models import Matches, Leagues, StreamSources
 from database import db
 from datetime import datetime, timedelta
 import pytz
-from streamSourceCrawler import get_live_links, get_stream_sources
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-SCRAPER_API_KEY = '6be10ab64669a2f166584ccf985109c6'
-scraper_api_url = "http://api.scraperapi.com"   
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DB_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DB_URL') if environ.get(
+    'DB_URL') else 'postgresql://postgres:postgres@localhost:5436/postgres'
 db.init_app(app)
+
 mongolia_tz = pytz.timezone('Asia/Ulaanbaatar')
 
 CORS(app, supports_credentials=True)
+
 
 @app.route('/matches', methods=['POST'])
 def create_match():
     try:
         data = request.get_json()
-        required_fields = ['team1name', 'team2name', 'link', 'time', 'date', 'league_id', 'datetime']
+        required_fields = ['team1name', 'team2name',
+                           'link', 'time', 'date', 'league_id', 'datetime']
         if not all(field in data for field in required_fields):
             return make_response(jsonify({'message': 'Missing required fields'}), 400)
-        
+
         new_match = Matches(**data)
         db.session.add(new_match)
         db.session.commit()
@@ -44,6 +43,7 @@ def get_matches():
     except Exception as e:
         return make_response(jsonify({'message': 'Error getting matches', 'error': str(e)}), 500)
 
+
 @app.route('/all-matches', methods=['GET'])
 def get_allmatches():
     try:
@@ -51,6 +51,7 @@ def get_allmatches():
         return make_response(jsonify([match.json() for match in all_matches]), 200)
     except Exception as e:
         return make_response(jsonify({'message': 'Error getting matches', 'error': str(e)}), 500)
+
 
 @app.route('/stream_sources', methods=['GET'])
 def get_streamsources():
@@ -60,13 +61,18 @@ def get_streamsources():
     except Exception as e:
         return make_response(jsonify({'message': 'Error getting stream sources', 'error': str(e)}), 500)
 
+
 @app.route('/live-matches', methods=['GET'])
 def get_live_matches():
     try:
-        live_matches = Matches.query.filter_by(isLive=True).all()
+        live_matches = Matches.query.filter(
+            Matches.datetime <= datetime.now(),
+            Matches.expected_end_at >= datetime.now()
+        ).all()
         return make_response(jsonify([match.json() for match in live_matches]), 200)
     except Exception as e:
         return make_response(jsonify({'message': 'Error getting live matches', 'error': str(e)}), 500)
+
 
 @app.route('/leagues', methods=['GET'])
 def get_leagues():
@@ -75,6 +81,7 @@ def get_leagues():
         return make_response(jsonify([league.json() for league in leagues]), 200)
     except Exception as e:
         return make_response(jsonify({'message': 'Error getting leagues', 'error': str(e)}), 500)
+
 
 @app.route('/matches/<int:id>', methods=['GET'])
 def get_match(id):
@@ -85,6 +92,7 @@ def get_match(id):
         return make_response(jsonify({'message': 'Match not found'}), 404)
     except Exception as e:
         return make_response(jsonify({'message': 'Error getting match', 'error': str(e)}), 500)
+
 
 @app.route('/matches/<int:id>', methods=['PUT'])
 def update_match(id):
@@ -100,6 +108,7 @@ def update_match(id):
     except Exception as e:
         return make_response(jsonify({'message': 'Error updating match', 'error': str(e)}), 500)
 
+
 @app.route('/matches/<int:id>', methods=['DELETE'])
 def delete_match(id):
     try:
@@ -112,11 +121,12 @@ def delete_match(id):
     except Exception as e:
         return make_response(jsonify({'message': 'Error deleting match', 'error': str(e)}), 500)
 
+
 @app.route('/matches/by-league', methods=['GET'])
 def get_matches_by_league():
     try:
         app.logger.info("Received request for matches by league")
-        
+
         league_name = request.args.get('league_name')
         app.logger.info(f"League name received: {league_name}")
 
@@ -128,7 +138,8 @@ def get_matches_by_league():
         if not league:
             app.logger.warning(f"League not found: {league_name}")
             return make_response(jsonify({'message': 'League not found'}), 404)
-        current_time_mongolia = datetime.now(mongolia_tz).replace(microsecond=0, tzinfo=None)
+        current_time_mongolia = datetime.now(
+            mongolia_tz).replace(microsecond=0, tzinfo=None)
         # Get the current time and subtract 3 hours
         three_hours_ago = current_time_mongolia - timedelta(hours=3)
 
@@ -137,7 +148,7 @@ def get_matches_by_league():
             Matches.league_id == league.id,
             Matches.datetime >= three_hours_ago
         ).all()
-        
+
         return make_response(jsonify([match.json() for match in matches]), 200)
     except Exception as e:
         app.logger.error(f"Error getting matches: {str(e)}")
@@ -150,62 +161,8 @@ def get_stream_sources_for_match(match_id):
         match = Matches.query.get(match_id)
         if not match:
             return make_response(jsonify({'message': 'Match not found'}), 404)
-
         if not match.isLive:
             return make_response(jsonify({'message': 'Match is not live'}), 200)
-        
-        last_crawl_time = match.last_crawl_time or datetime.min
-
-        # If the match is already being crawled or was recently crawled, return existing sources
-        if  match.isCrawling or (datetime.now() - last_crawl_time).total_seconds() < 300:
-            
-            app.logger.info("Returning existing stream sources due to recent crawl")
-            existing_sources = StreamSources.query.filter_by(match_id=match_id).all()
-            return make_response(jsonify([source.json() for source in existing_sources]), 200)
-        
-
-        # Set isCrawling to True to indicate a new crawl is in progress
-        match.isCrawling = True
-        db.session.commit()
-        # Start the crawler to get live links
-        chrome_options = Options()
-        chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--proxy-bypass-list=*")
-        chrome_options.add_argument("--disable-browser-side-navigation")
-        chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.add_argument("--remote-debugging-port=9222")
-        chrome_options.add_argument('--no-zygote')
-        chrome_options.add_argument("--disable-gpu") 
-        
-        target_url = 'https://sportshub.stream'
-        proxy = f"{scraper_api_url}?api_key={SCRAPER_API_KEY}&url={target_url}"
-        chrome_options.add_argument(f'--proxy-server={proxy}')
-        driver = webdriver.Chrome(options=chrome_options)
-        stream_links = get_live_links(driver, match.link)
-        new_sources = []
-
-        if stream_links:
-            for link in stream_links:
-                # Check if the link already exists in the database
-                existing_source = StreamSources.query.filter_by(match_id=match_id, link=link).first()
-                if not existing_source:
-                    app.logger.info("ADDED NEW SOURCE")
-                    # Crawl and add new sources only if the link is new
-                    new_source_links = get_stream_sources(driver, [link])
-                    for source in new_source_links:
-                        new_source = StreamSources(match_id=match_id, link=link, source=source)
-                        db.session.add(new_source)
-                        new_sources.append(new_source)
-
-            # Update last crawl time and reset isCrawling status
-            match.last_crawl_time = datetime.now()
-            db.session.commit()
-
-        match.isCrawling = False
-        db.session.commit()
-        # Get all sources (both new and existing) and return them
         all_sources = StreamSources.query.filter_by(match_id=match_id).all()
         return make_response(jsonify([source.json() for source in all_sources]), 200)
 
@@ -213,12 +170,6 @@ def get_stream_sources_for_match(match_id):
         app.logger.error(f"Error getting stream sources for match: {str(e)}")
         return make_response(jsonify({'message': 'Error getting stream sources', 'error': str(e)}), 500)
 
-import matchCrawler
-import streamSourceCrawler
-app.logger.info("main")
-with app.app_context():
-    db.create_all()
-    matchCrawler.main_loop(app, db)
-    streamSourceCrawler.main_loop(app, db)
-    
+
+if __name__ == '__main__':
     app.run(debug=False)
--- models.py
+++ models.py
@@ -1,22 +1,27 @@
 from database import db
+from datetime import datetime
+
 
 class Leagues(db.Model):
     __tablename__ = 'leagues'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String, nullable=False)
     url = db.Column(db.String, nullable=False)
-    
+    created_at = db.Column(db.DateTime, nullable=True)
+
     matches = db.relationship("Matches", back_populates="league")
 
-    def __init__(self, name, url):
+    def __init__(self, name, url, created_at):
         self.name = name
         self.url = url
+        self.created_at = created_at
 
     def __repr__(self):
         return f"<Leagues(name='{self.name}', url='{self.url}')>"
 
     def json(self):
         return {'id': self.id, 'name': self.name, 'url': self.url}
+
 
 class StreamSources(db.Model):
     __tablename__ = 'stream_sources'
@@ -26,7 +31,7 @@ class StreamSources(db.Model):
     source = db.Column(db.String)
 
     match = db.relationship("Matches", back_populates="stream_sources")
-    
+
     def __init__(self, match_id, link, source):
         self.match_id = match_id
         self.link = link
@@ -35,40 +40,54 @@ class StreamSources(db.Model):
     def json(self):
         return {'id': self.id, 'match_id': self.match_id, 'link': self.link, 'source': self.source}
 
+
 class Matches(db.Model):
     __tablename__ = 'matches'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    team1name = db.Column(db.String, nullable=True)
-    team2name = db.Column(db.String, nullable=True)
+    team1name = db.Column(db.String, nullable=True, index=True)
+    team2name = db.Column(db.String, nullable=True, index=True)
     link = db.Column(db.String, nullable=True)
     time = db.Column(db.String, nullable=True)
     date = db.Column(db.String, nullable=True)
-    league_id = db.Column(db.Integer, db.ForeignKey('leagues.id'))
+    league_id = db.Column(db.Integer, db.ForeignKey('leagues.id'), index=True)
     datetime = db.Column(db.DateTime, nullable=True)
-    isLive = db.Column(db.Boolean, nullable=False, default=False)
-    description = db.Column(db.String, nullable=True) 
-    last_crawl_time = db.Column(db.DateTime, nullable=True, default=None) 
+    expected_end_at = db.Column(db.DateTime, nullable=True)
+    live_at = db.Column(db.DateTime, nullable=True)
+    live_end_at = db.Column(db.DateTime, nullable=True)
+    created_at = db.Column(db.DateTime, nullable=True)
+    description = db.Column(db.String, nullable=True)
+    last_crawl_time = db.Column(db.DateTime, nullable=True, default=None)
     isCrawling = db.Column(db.Boolean, nullable=False, default=False)
-    
-    league = db.relationship("Leagues", back_populates="matches")
-    stream_sources = db.relationship("StreamSources", back_populates="match", cascade="all, delete-orphan")
 
-    def __init__(self, team1name, team2name, link, time, date, league_id, datetime, isLive=False, description=None, last_crawl_time=None, isCrawling=False):
+    league = db.relationship("Leagues", back_populates="matches")
+    stream_sources = db.relationship(
+        "StreamSources", back_populates="match", cascade="all, delete-orphan")
+
+    def __init__(self, team1name, team2name, link, time, date, league_id, datetime, live_at=None, description=None, last_crawl_time=None, isCrawling=False, created_at=None, live_end_at=None, expected_end_at=None):
         self.team1name = team1name
         self.team2name = team2name
         self.link = link
         # match's current time
-        self.time = time 
+        self.time = time
         # match created time
         self.date = date
         self.league_id = league_id
         # match start time
         self.datetime = datetime
-        self.isLive = isLive
+        self.expected_end_at = expected_end_at
+        self.live_at = live_at
+        self.live_end_at = live_end_at
+        self.created_at = created_at
         self.description = description
         self.last_crawl_time = last_crawl_time
         self.isCrawling = isCrawling
 
+    @property
+    def isLive(self):
+        """Returns True if the match is live, based on the current time."""
+        current_time = datetime.now()
+        # Check if the current time is within the match start and expected end times
+        return self.datetime <= current_time <= self.expected_end_at
 
     def json(self):
         return {
@@ -80,13 +99,14 @@ class Matches(db.Model):
             'date': self.date,
             'league_id': self.league_id,
             'datetime': self.datetime,
-            'isLive': self.isLive,
+            'expected_end_at': self.expected_end_at,
+            'live_at': self.live_at,
+            'live_end_at': self.live_end_at,
             'description': self.description,
             'last_crawl_time': self.last_crawl_time,
             'isCrawling': self.isCrawling,
-            
+            'isLive': self.isLive,
         }
 
     def __repr__(self):
-        return f"<Matches(id={self.id}, team1name='{self.team1name}', team2name='{self.team2name}', isLive={self.isLive}, description='{self.description}', last_crawl_time='{self.last_crawl_time}', datetime='{self.datetime}')>"
-
+        return f"<Matches(id={self.id}, team1name='{self.team1name}', team2name='{self.team2name}', live_at={self.live_at}, description='{self.description}', last_crawl_time='{self.last_crawl_time}', datetime='{self.datetime}')>"
